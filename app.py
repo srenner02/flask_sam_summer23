1884657a9cf97fb9f1be0bf1f5bb77466bcd3e8f
Sam Renner <srenner02@gmail.com>
2023-06-30 14:45:31 +0000

    Main Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/contact")
+def home():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run()
